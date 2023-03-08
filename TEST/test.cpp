6ea3d18f754dc30bdd1cb133e237f0123216172c
BrotherHong <leo361288@gmail.com>
2023-03-08 17:50:31 +0800

    test

---
+++ TEST/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "tool.h"
+using namespace std;
+
+int main() {
+	cout << "Hello\n";
+	sayHi();
+	return 0;	
+}
---
+++ TEST/tool.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "tool.h"
+using namespace std;
+
+void sayHi() {
+	cout << "Hey You\n";	
+}
+	
